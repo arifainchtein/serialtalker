90ee32c4c6d502a7cd4b64ebe074eb0c8cf58d10
Ari Fainchtein <ari@digitalgeppetto.com>
2018-10-12 15:36:26 +1100

    adding single command exeuction

--- src/main/java/com/teleonome/serialtalker/OneCommandExecution.java
+++ src/main/java/com/teleonome/serialtalker/OneCommandExecution.java
@@ -70,7 +70,7 @@ public class OneCommandExecution {
 					}else {
 						cleaned=line;
 					}
-					if(verbose) System.out.println(cleaned);
+					System.out.println(cleaned);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
